a7e39a4e826146b5d34e8e4e8335a46e708ce7f8
carrieyzs <cary.0315@gmail.com>
2018-09-20 00:56:22 +1200

    Created the class that implements the abstract GUI class.

---
+++ src/Application/GameWorldApp.java
@@ -0,0 +1,19 @@
+package Application;
+
+public class GameWorldApp extends GUI {
+
+	public GameWorldApp() {
+
+	}
+
+	@Override
+	protected void onStart() {
+		// System.out.println("Test");
+	}
+
+	public static void main(String[] args) {
+		new GameWorldApp();
+	}
+
+
+}
