1643b951252f88ee863d9d3bd62bf7a7900dfa4f
Deanne Alabastro <aladeanra@gmail.com>
2018-09-21 13:28:36 +1200

    Create Abstract Item Class

---
+++ src/gameworld/Item.java
@@ -0,0 +1,32 @@
+package gameworld;
+
+/**
+ * @author Deanne Alabastro
+ *
+ */
+public abstract class Item {
+
+	private Location location;
+
+	/**
+	 * 
+	 */
+	public Item(Location location) {
+		this.location = location;
+	}
+	
+	public String examine() {
+		return toString();
+	}
+	
+	public Location getLocation() {
+		return this.location;
+	}
+	
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	
+	public abstract String toString();
+
+}
