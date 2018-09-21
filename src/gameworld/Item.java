ad10ecdf0c91268bc4d75fdb660931384392acaa
carrieyzs <cary.0315@gmail.com>
2018-09-21 15:45:47 +1200

    Comment on action(). Maybe allow action to accept String arguments for user actions, in the future.

--- src/gameworld/Item.java
+++ src/gameworld/Item.java
@@ -9,24 +9,26 @@ public abstract class Item {
 	private Location location;
 
 	/**
-	 * 
+	 *
 	 */
 	public Item(Location location) {
 		this.location = location;
 	}
-	
+
 	public String examine() {
 		return toString();
 	}
-	
+
 	public Location getLocation() {
 		return this.location;
 	}
-	
+
 	public void setLocation(Location location) {
 		this.location = location;
 	}
-	
+
 	public abstract String toString();
+
+	public abstract void action();			// String argument?
 
 }
