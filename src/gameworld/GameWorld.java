f09c442d57c375184ba6900c32db8e990ce60ab0
Deanne Alabastro <aladeanra@gmail.com>
2018-10-04 22:10:41 +1300

    Update GameWorld

--- src/gameworld/GameWorld.java
+++ src/gameworld/GameWorld.java
@@ -2,10 +2,69 @@ package gameworld;
 
 import controller.Controller;
 
+/**
+ * GameWorld handles all the entities and items.
+ */
 public class GameWorld {
+	
+	/** The controller. */
 	private Controller controller;
 	
+	/**
+	 * Instantiates a new game world.
+	 *
+	 * @param controller the controller
+	 */
 	public GameWorld(Controller controller) {
+		this.controller = controller;
+	}
+	
+	/**
+	 * Update.
+	 */
+	public void update() {
+		
+	}
+	
+	/**
+	 * Render.
+	 */
+	public void render() {
+		
+	}
+	
+	/**
+	 * Load game world.
+	 *
+	 * @param path the path
+	 */
+	private void loadGameWorld(String path){
+		
+	}
+	
+	/**
+	 * Save game world.
+	 */
+	private void SaveGameWorld(){
+		// should return something. Maybe a file? or maybe not return something.
+		// up to you bennette :)
+	}
+	
+	/**
+	 * Gets the controller.
+	 *
+	 * @return the controller
+	 */
+	public Controller getController() {
+		return controller;
+	}
+
+	/**
+	 * Sets the controller.
+	 *
+	 * @param controller the new controller
+	 */
+	public void setController(Controller controller) {
 		this.controller = controller;
 	}
 }
