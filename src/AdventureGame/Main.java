5ab3a3b617b290bfb7201de11432b17e334318c8
Deanne Alabastro <aladeanra@gmail.com>
2018-10-04 22:02:19 +1300

    create adventure game package which contains main class. This is used to launch the game

---
+++ src/AdventureGame/Main.java
@@ -0,0 +1,18 @@
+package AdventureGame;
+
+/**
+ * Launches the Adventure Game.
+ */
+public class Main {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		AdventureGame game = new AdventureGame();
+		game.start(); // starts the game thread.
+	}
+
+}
