238428e518ba5cb564b7e7bc36bdecc1e62a6f3b
Deanne Alabastro <aladeanra@gmail.com>
2018-10-05 11:46:24 +1300

    create FighterEntity abstract class. This is what player, monster, and any other organism that can fight back extend

--- src/gameworld/entities/FighterEntity.java
+++ src/gameworld/entities/FighterEntity.java
@@ -3,20 +3,34 @@ package gameworld.entities;
 import controller.Controller;
 import gameworld.Location;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class FighterEntity.
+ */
 public abstract class FighterEntity extends Entity {
 	
+	/** The min damage. */
 	protected int minDamage;
+	
+	/** The max damage. */
 	protected int maxDamage;
 	
 
 	/**
-	 * @param controller
-	 * @param location
+	 * Instantiates a new fighter entity.
+	 *
+	 * @param controller the controller
+	 * @param location the location
 	 */
 	public FighterEntity(Controller controller, Location location) {
 		super(controller, location);
 	}
 	
+	/**
+	 * Attack.
+	 *
+	 * @param opponent the opponent
+	 */
 	abstract protected void attack(Entity opponent);
 	
 }
