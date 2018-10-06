9d1a239799c4ae5ce89c2996878f1da15ac65c29
Raphael Namay <Jio@jios-mbp.hub>
2018-10-07 01:02:29 +1300

    Added bathroom tile

---
+++ src/gameworld/tiles/BathroomTile.java
@@ -0,0 +1,11 @@
+package gameworld.tiles;
+
+import java.awt.image.BufferedImage;
+
+public class BathroomTile extends Tile {
+
+	public BathroomTile(int id) {
+		super(Assets.bathroom, id);
+	}
+
+}
