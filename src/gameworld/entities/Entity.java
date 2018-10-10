1fc4bf33111915f58cd275a5f294fe13296a04d3
Deanne Alabastro <aladeanra@gmail.com>
2018-10-10 23:01:36 +1300

    all entities can get damaged but some cannot attack. Created attackingEntity class for entities who have attacking capabilities

--- src/gameworld/entities/Entity.java
+++ src/gameworld/entities/Entity.java
@@ -1,8 +1,6 @@
 package gameworld.entities;
-
-import java.util.Random;
-
 import gameworld.GameObjectInterface;
+import gameworld.Location;
 
 /**
  * The Entity class includes any attackable thing that may optionally drop items
@@ -10,14 +8,41 @@ import gameworld.GameObjectInterface;
  */
 public abstract class Entity implements GameObjectInterface {
 	
-	// ********** ATTACKING METHODS ********** //
-	
 	/** The max damage. */
 	protected int maxDamage;
 	
 	/** The health. */
 	protected int health;
+
+	/**
+	 * @return the health
+	 */
+	public int getHealth() {
+		return health;
+	}
+
+	/**
+	 * @param health the health to set
+	 */
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
 	
+	/**
+	 * @return the maxDamage
+	 */
+	public int getMaxDamage() {
+		return maxDamage;
+	}
+
+	/**
+	 * @param maxDamage the maxDamage to set
+	 */
+	public void setMaxDamage(int maxDamage) {
+		this.maxDamage = maxDamage;
+	}
+
 	/**
 	 * Decrease the object's health.
 	 *
@@ -31,55 +56,6 @@ public abstract class Entity implements GameObjectInterface {
 		}else {
 			health -= amount;
 		}
-	}
-
-	/**
-	 * Attack.
-	 *
-	 * @param opponent
-	 *            the opponent
-	 */
-	protected boolean attack(Entity opponent) {
-		int attackDamage = calculateAttackDamage();
-		if (attackDamage == 0)
-			return false;
-		else {
-			opponent.getDamaged(attackDamage);
-			return true;
-		}
-	}
-
-	/**
-	 * Generate random attack damage.
-	 *
-	 * @return random attack damage to be used in calculating actual attack damage
-	 */
-	private int generateRandomDamage() {
-		int damage = 0;
-		int max = maxDamage / 2; // makes sure calculated damage does not go above maxDamage
-		for (int i = 0; i < 2; i++) {
-			damage += new Random().nextInt(max + 1);
-		}
-		return damage;
-	}
-
-	/**
-	 * Calculates attack damage by randomly generating a number between the damage
-	 * range. Can also be 0 which means the attack had no effect or missed.
-	 *
-	 * @return attack damage to be used in attack
-	 */
-	protected int calculateAttackDamage() {
-		// generate the entity's attack damage, taking the min between two possible
-		// random attack damages
-		int damage = Math.min(generateRandomDamage(), generateRandomDamage());
-
-		// with 5% chance, check to see if entity can increase attack damage through a
-		// "critical hit"
-		if (new Random().nextInt(101) < 5)
-			damage += generateRandomDamage();
-
-		return damage;
 	}
 	
 	// ********** ABSTRACT METHODS ********** //
