b1a96afc6996693f35d00668563dc2b10dbe5286
Raphael Namay <Jio@Jios-MBP.hub>
2018-10-12 02:47:17 +1300

    Added buttons to Map Editor GUI; edited initial game map in MapEditor.java

--- src/mapeditor/MapEditorGUI.java
+++ src/mapeditor/MapEditorGUI.java
@@ -3,25 +3,42 @@ package mapeditor;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
-public abstract class MapEditorGUI {
+public class MapEditorGUI {
 
 	private JFrame frame;
 	private JPanel boardPanel;
 	private JPanel buttonPanel;
-	private JButton entityButton;
-	// private JButton
+	private JButton rockButton;
+	private JButton sofaButton;
+	private JButton tableButton;
+	private JButton treeButton;
+	private JButton noteButton;
+	private JButton bedButton;
+	private JButton fountainButton;
+	private JButton cactusButton;
+	private JButton bookshelfButton;
+	private JButton treasureChestButton;
+	private JButton wallBlockButton;
+	private JButton doorButton;
+	private JLabel roomSelectLabel;
 	private JMenuBar menuBar;
 	private JMenu fileMenu;
 	private JMenuItem saveMenuItem;
 	private JMenuItem loadMenuItem;
+
+	String roomSelected = "Library";
 
 	public MapEditorGUI() {
 		GUI();
@@ -30,7 +47,12 @@ public abstract class MapEditorGUI {
 	public void GUI() {
 
 		frame = new JFrame("Map Editor");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLayout(new BorderLayout());
+		frame.setSize(700, 500);
 
+		/* MENU BAR */
 		menuBar = new JMenuBar();
 		fileMenu = new JMenu("File");
 		saveMenuItem = new JMenuItem("Save");
@@ -39,46 +61,116 @@ public abstract class MapEditorGUI {
 		fileMenu.add(loadMenuItem);
 		menuBar.add(fileMenu);
 		frame.setJMenuBar(menuBar);
+		/* MENU BAR */
 
-		frame.setLayout(new BorderLayout());
-		frame.setSize(500, 450);
+		/* BOARD */
 		boardPanel = new BoardPanel();
+		boardPanel.setBounds(0, 0, 280, 280);
 		frame.add(boardPanel, BorderLayout.CENTER);
-		entityButton = new JButton("Entity");
+		/* BOARD */
+
+		/* BUTTON PANEL */
 		buttonPanel = new JPanel();
-		buttonPanel.add(entityButton);
-		frame.add(buttonPanel, BorderLayout.EAST);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		buttonPanel.setLayout(null);
+		/* BUTTON PANEL */
+
+		/* ROOM SELECTION */
+		roomSelectLabel = new JLabel("Select room to edit:");
+
+		String[] roomStrings = { "Library", "Guest Bedroom", "Foyer", "Courtyard", "Master Bedroom", "Kitchen" };
+
+		JComboBox petList = new JComboBox(roomStrings);
+		petList.setSelectedIndex(0);
+		petList.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JComboBox cb = (JComboBox) e.getSource();
+				String roomName = (String) cb.getSelectedItem();
+				roomSelected = roomName;
+			}
+
+		});
+
+		roomSelectLabel.setBounds(300, 0, 500, 60);
+		buttonPanel.add(roomSelectLabel);
+		petList.setBounds(427, 0, 170, 60);
+		buttonPanel.add(petList);
+		/* ROOM SELECTION */
+
+		rockButton = new JButton("Rock (contains key)");
+		rockButton.setBounds(300, 60, 170, 60);
+		buttonPanel.add(rockButton);
+
+		sofaButton = new JButton("Sofa");
+		sofaButton.setBounds(470, 60, 170, 60);
+		buttonPanel.add(sofaButton);
+
+		tableButton = new JButton("Table");
+		tableButton.setBounds(300, 120, 170, 60);
+		buttonPanel.add(tableButton);
+
+		treeButton = new JButton("Tree");
+		treeButton.setBounds(470, 120, 170, 60);
+		buttonPanel.add(treeButton);
+
+		noteButton = new JButton("Note");
+		noteButton.setBounds(470, 120, 170, 60);
+		buttonPanel.add(noteButton);
+
+		bedButton = new JButton("Bed");
+		bedButton.setBounds(300, 180, 170, 60);
+		buttonPanel.add(bedButton);
+
+		fountainButton = new JButton("Fountain");
+		fountainButton.setBounds(470, 180, 170, 60);
+		buttonPanel.add(fountainButton);
+
+		cactusButton = new JButton("Cactus");
+		cactusButton.setBounds(300, 240, 170, 60);
+		buttonPanel.add(cactusButton);
+
+		bookshelfButton = new JButton("Bookshelf");
+		bookshelfButton.setBounds(470, 240, 170, 60);
+		buttonPanel.add(bookshelfButton);
+
+		treasureChestButton = new JButton("Treasure Chest");
+		treasureChestButton.setBounds(300, 300, 170, 60);
+		buttonPanel.add(treasureChestButton);
+
+		wallBlockButton = new JButton("Wall Block");
+		wallBlockButton.setBounds(470, 300, 170, 60);
+		buttonPanel.add(wallBlockButton);
+
+		doorButton = new JButton("Door");
+		doorButton.setBounds(385, 360, 170, 60);
+		buttonPanel.add(doorButton);
+
+		frame.add(buttonPanel);
 		frame.setVisible(true);
-		frame.setResizable(false);
 	}
 
 	private class BoardPanel extends JPanel {
 
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 4409893495609306741L;
-
 		public void paintComponent(Graphics g) {
 
 			g.setColor(Color.white);
-			g.fillRect(0, 0, 10 * 32, 10 * 32);
+			g.fillRect(0, 0, 7 * 40, 7 * 40);
 			g.setColor(Color.gray);
 
-			for (int i = 0; i < 10; i++) {
-				g.drawLine(i * 32, 0, i * 32, 10 * 32);
+			for (int i = 0; i < 7; i++) {
+				g.drawLine(i * 40, 0, i * 40, 7 * 40);
 			}
 
-			for (int j = 0; j < 10; j++) {
-				g.drawLine(0, j * 32, 10 * 32, j * 32);
+			for (int j = 0; j < 7; j++) {
+				g.drawLine(0, j * 40, 7 * 40, j * 40);
 			}
 
 			g.setColor(Color.black);
-			g.drawLine(0, 0, 10 * 32, 0);
-			g.drawLine(0, 0, 0, 10 * 32);
-			g.drawLine(10 * 32, 0, 10 * 32, 10 * 32);
-			g.drawLine(0, 10 * 32, 10 * 32, 10 * 32);
+			g.drawLine(0, 0, 7 * 40, 0);
+			g.drawLine(0, 0, 0, 7 * 40);
+			g.drawLine(7 * 40, 0, 7 * 40, 7 * 40);
+			g.drawLine(0, 7 * 40, 7 * 40, 7 * 40);
 		}
 
 	}
