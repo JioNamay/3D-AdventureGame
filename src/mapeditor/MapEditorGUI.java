ef05e00f8021461f8bfa475952a90b3aee0b965e
Raphael Namay <Jio@Jios-MBP.hub>
2018-10-14 05:48:49 +1300

    Map on GUI now also represented by a 2D array in real-time for exports; 'Note' button now visible; Buttons given corresponding numbers for item placement; Added delete mode

--- src/mapeditor/MapEditorGUI.java
+++ src/mapeditor/MapEditorGUI.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,8 +12,12 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -22,6 +27,8 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+
+import gameworld.tiles.Assets;
 
 public class MapEditorGUI {
 
@@ -40,26 +47,31 @@ public class MapEditorGUI {
 	private JButton treasureChestButton;
 	private JButton wallBlockButton;
 	private JButton doorButton;
+	private JButton deleteButton;
 	private JLabel roomSelectLabel;
 	private JMenuBar menuBar;
 	private JMenu fileMenu;
 	private JMenuItem saveMenuItem;
 	private JMenuItem loadMenuItem;
 
-	String roomSelected = "Library";
-	String selectedItem = " ";
+	protected String roomSelected = "Library";
+	protected String selectedItem = " ";
+	protected boolean deleteMode = false;
 
-	private char[][] map;
+	private int[][] roomMap;
+
+	private mapeditor.MapEditorGUI.BoardPanel.BoardPanelComponent pressedOn;
+	private mapeditor.MapEditorGUI.BoardPanel.BoardPanelComponent releasedOn;
 
 	private ArrayList<mapeditor.MapEditorGUI.BoardPanel.BoardPanelComponent> boardTiles;
 
 	public MapEditorGUI() {
 		boardTiles = new ArrayList<mapeditor.MapEditorGUI.BoardPanel.BoardPanelComponent>();
 
-		map = new char[7][7];
+		roomMap = new int[7][7];
 		for (int i = 0; i < 7; i++) {
 			for (int j = 0; j < 7; j++) {
-				map[i][j] = ' ';
+				roomMap[i][j] = 0;
 			}
 		}
 		GUI();
@@ -129,13 +141,14 @@ public class MapEditorGUI {
 		buttonPanel.add(petList);
 		/* ROOM SELECTION */
 
-		rockButton = new JButton("Rock (contains key)");
+		rockButton = new JButton("[1] Rock (contains key)");
 		rockButton.setBounds(300, 60, 170, 60);
 		rockButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectedItem = "rock";
+				turnDeleteModeOff();
 				// (selectedItem);
 				boardPanel.repaint();
 			}
@@ -143,13 +156,14 @@ public class MapEditorGUI {
 		});
 		buttonPanel.add(rockButton);
 
-		sofaButton = new JButton("Sofa");
+		sofaButton = new JButton("[2] Sofa");
 		sofaButton.setBounds(470, 60, 170, 60);
 		sofaButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectedItem = "sofa";
+				turnDeleteModeOff();
 				// (selectedItem);
 				boardPanel.repaint();
 			}
@@ -157,13 +171,14 @@ public class MapEditorGUI {
 		});
 		buttonPanel.add(sofaButton);
 
-		tableButton = new JButton("Table");
+		tableButton = new JButton("[3] Table");
 		tableButton.setBounds(300, 120, 170, 60);
 		tableButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectedItem = "table";
+				turnDeleteModeOff();
 				// (selectedItem);
 				boardPanel.repaint();
 			}
@@ -171,13 +186,14 @@ public class MapEditorGUI {
 		});
 		buttonPanel.add(tableButton);
 
-		treeButton = new JButton("Tree");
+		treeButton = new JButton("[4] Tree");
 		treeButton.setBounds(470, 120, 170, 60);
 		treeButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectedItem = "tree";
+				turnDeleteModeOff();
 				// (selectedItem);
 				boardPanel.repaint();
 			}
@@ -185,27 +201,14 @@ public class MapEditorGUI {
 		});
 		buttonPanel.add(treeButton);
 
-		noteButton = new JButton("Note");
-		noteButton.setBounds(470, 120, 170, 60);
-		noteButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				selectedItem = "note";
-				// (selectedItem);
-				boardPanel.repaint();
-			}
-
-		});
-		buttonPanel.add(noteButton);
-
-		bedButton = new JButton("Bed");
+		bedButton = new JButton("[5] Bed");
 		bedButton.setBounds(300, 180, 170, 60);
 		bedButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectedItem = "bed";
+				turnDeleteModeOff();
 				// (selectedItem);
 				boardPanel.repaint();
 			}
@@ -213,13 +216,14 @@ public class MapEditorGUI {
 		});
 		buttonPanel.add(bedButton);
 
-		fountainButton = new JButton("Fountain");
+		fountainButton = new JButton("[6] Fountain");
 		fountainButton.setBounds(470, 180, 170, 60);
 		fountainButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectedItem = "fountain";
+				turnDeleteModeOff();
 				// (selectedItem);
 				boardPanel.repaint();
 			}
@@ -227,13 +231,14 @@ public class MapEditorGUI {
 		});
 		buttonPanel.add(fountainButton);
 
-		cactusButton = new JButton("Cactus");
+		cactusButton = new JButton("[7] Cactus");
 		cactusButton.setBounds(300, 240, 170, 60);
 		cactusButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectedItem = "cactus";
+				turnDeleteModeOff();
 				// (selectedItem);
 				boardPanel.repaint();
 			}
@@ -241,13 +246,14 @@ public class MapEditorGUI {
 		});
 		buttonPanel.add(cactusButton);
 
-		bookshelfButton = new JButton("Bookshelf");
+		bookshelfButton = new JButton("[8] Bookshelf");
 		bookshelfButton.setBounds(470, 240, 170, 60);
 		bookshelfButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectedItem = "bookshelf";
+				turnDeleteModeOff();
 				// (selectedItem);
 				boardPanel.repaint();
 			}
@@ -255,13 +261,14 @@ public class MapEditorGUI {
 		});
 		buttonPanel.add(bookshelfButton);
 
-		treasureChestButton = new JButton("Treasure Chest");
+		treasureChestButton = new JButton("[9] Treasure Chest");
 		treasureChestButton.setBounds(300, 300, 170, 60);
 		treasureChestButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectedItem = "treasure chest";
+				turnDeleteModeOff();
 				// (selectedItem);
 				boardPanel.repaint();
 			}
@@ -269,13 +276,14 @@ public class MapEditorGUI {
 		});
 		buttonPanel.add(treasureChestButton);
 
-		wallBlockButton = new JButton("Wall Block");
+		wallBlockButton = new JButton("[10] Wall Block");
 		wallBlockButton.setBounds(470, 300, 170, 60);
 		wallBlockButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectedItem = "wall block";
+				turnDeleteModeOff();
 				// (selectedItem);
 				boardPanel.repaint();
 			}
@@ -283,13 +291,29 @@ public class MapEditorGUI {
 		});
 		buttonPanel.add(wallBlockButton);
 
-		doorButton = new JButton("Door");
-		doorButton.setBounds(385, 360, 170, 60);
+		noteButton = new JButton("[11] Note");
+		noteButton.setBounds(300, 360, 170, 60);
+		noteButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				selectedItem = "note";
+				turnDeleteModeOff();
+				// (selectedItem);
+				boardPanel.repaint();
+			}
+
+		});
+		buttonPanel.add(noteButton);
+
+		doorButton = new JButton("[12] Door");
+		doorButton.setBounds(470, 360, 170, 60);
 		doorButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectedItem = "door";
+				turnDeleteModeOff();
 				// (selectedItem);
 				boardPanel.repaint();
 			}
@@ -297,9 +321,62 @@ public class MapEditorGUI {
 		});
 		buttonPanel.add(doorButton);
 
+		deleteButton = new JButton("Delete Mode: Off");
+		deleteButton.setBounds(40 * 7 / 2 - 170 / 2, 40 * 7 + 30, 170, 60);
+		deleteButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				deleteMode = !deleteMode;
+				if (deleteMode == true) {
+					selectedItem = " ";
+					deleteButton.setText("Delete Mode: On");
+					boardPanel.repaint();
+				} else {
+					deleteButton.setText("Delete Mode: Off");
+				}
+			}
+
+		});
+		buttonPanel.add(deleteButton);
+
 		frame.add(buttonPanel);
 		frame.setVisible(true);
-		// boardPanel.setBac
+	}
+
+	public void turnDeleteModeOff() {
+		deleteMode = false;
+		deleteButton.setText("Delete Mode: Off");
+	}
+
+	public int getItemAsInt(String item) {
+		switch (item) {
+		case "rock":
+			return 1;
+		case "sofa":
+			return 2;
+		case "table":
+			return 3;
+		case "tree":
+			return 4;
+		case "bed":
+			return 5;
+		case "fountain":
+			return 6;
+		case "cactus":
+			return 7;
+		case "bookshelf":
+			return 8;
+		case "treasure chest":
+			return 9;
+		case "wall block":
+			return 10;
+		case "note":
+			return 11;
+		case "door":
+			return 12;
+		}
+		return -1;
 	}
 
 	private class BoardPanel extends JLayeredPane {
@@ -307,9 +384,7 @@ public class MapEditorGUI {
 		public void addTiles() {
 			for (int i = 0; i < 7; i++) {
 				for (int j = 0; j < 7; j++) {
-					BoardPanelComponent bpc = new BoardPanelComponent(new Rectangle(j * 40, i * 40, 40, 40));
-					// bpc.setMinimumSize(new Dimension(40, 40));
-					// bpc.setPreferredSize(new Dimension(, 40));
+					BoardPanelComponent bpc = new BoardPanelComponent(new Rectangle(j * 40, i * 40, 40, 40), i, j);
 					boardPanel.add(bpc);
 					boardTiles.add(bpc);
 				}
@@ -361,46 +436,53 @@ public class MapEditorGUI {
 //			}
 		}
 
-		class BoardPanelComponent extends JLayeredPane implements MouseMotionListener, MouseListener {
+		class BoardPanelComponent extends JLayeredPane implements MouseListener {
 
 			private String item = " ";
+			public int x;
+			public int y;
 
-			public BoardPanelComponent(Rectangle bounds) {
+			public BoardPanelComponent(Rectangle bounds, int x, int y) {
+				this.x = x;
+				this.y = y;
 				setBounds(bounds);
 				addMouseListener(this);
-//				boardPanel.addMouseListener(this);
-//				frame.addMouseListener(this);
 			}
 
 			public void paintComponent(Graphics g) {
-				if (!selectedItem.equals(" ") && item.equals(" ")) {
+
+				if (!selectedItem.equals(" ") && roomMap[x][y] == 0) {
 					g.setColor(Color.CYAN);
-				} else {
+				} else if (selectedItem.equals(" ") && roomMap[x][y] == 0) {
 					g.setColor(Color.WHITE);
+				} else if (selectedItem.equals(" ")) {
+					g.setColor(Color.RED);
 				}
+
 				g.fillRect(getBounds().x, getBounds().y, getWidth(), getHeight());
 				g.setColor(Color.BLACK);
 				g.drawRect(getBounds().x, getBounds().y, getWidth(), getHeight());
 			}
 
 			@Override
-			public void mouseDragged(MouseEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mouseMoved(MouseEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
 			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println("click");
-				item = selectedItem;
-				repaint();
+
+				if (!selectedItem.equals(" ")) {
+					item = selectedItem;
+					roomMap[x][y] = getItemAsInt(selectedItem);
+					selectedItem = " ";
+					System.out.println(Arrays.deepToString(roomMap).replace("], ", "]\n"));
+					System.out.println("");
+					boardPanel.repaint();
+				}
+
+				if (deleteMode == true) {
+					roomMap[x][y] = 0;
+					System.out.println(Arrays.deepToString(roomMap).replace("], ", "]\n"));
+					System.out.println("");
+					boardPanel.repaint();
+				}
+
 			}
 
 			@Override
