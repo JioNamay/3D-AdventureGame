5645392f4dd6c254811402658c4e9040560e0da2
Deanne Alabastro <aladeanra@gmail.com>
2018-10-13 22:07:12 +1300

    Made wall class implement strategy, its only purpose is to set loc as solid so it doesnt return anything in its methods

--- src/gameworld/entities/Wall.java
+++ src/gameworld/entities/Wall.java
@@ -1,40 +1,63 @@
 package gameworld.entities;
 
-import gameworld.Location;
+import java.util.List;
+
+import gameworld.Location.Direction;
+import gameworld.entities.Item.Action;
 
 /**
  * The wall object represents a wall and marks a location as solid so that a player cannot walk past it.
  */
-public class Wall{
-	
-	/** The location. */
-	private Location location;
+public class Wall implements Strategy{
 
 	/**
 	 * Instantiates a new wall.
 	 *
 	 * @param location the location
 	 */
-	public Wall(Location location) {
-		this.setLocation(location);
-		location.setSolid(true);
+	public Wall() {
+		
 	}
 
-	/**
-	 * Gets the location.
-	 *
-	 * @return the location
-	 */
-	public Location getLocation() {
-		return location;
+	@Override
+	public List<String> getActions() {
+		return null;
 	}
 
-	/**
-	 * Sets the location.
-	 *
-	 * @param location the location to set
-	 */
-	public void setLocation(Location location) {
-		this.location = location;
+	@Override
+	public String getDescription() {
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public boolean isSolid() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isDoor() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Direction getDirection() {
+		return null;
+	}
+
+	@Override
+	public void setDirection(Direction direction) {
+	
+	}
+
+	@Override
+	public String performAction(Action action) {
+		return null;
 	}
 }
