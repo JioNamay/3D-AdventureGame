1ade99f514253f280753328889e46ac2979661cf
Deanne Alabastro <aladeanra@gmail.com>
2018-10-13 23:57:49 +1300

    Create tree and stick class

---
+++ src/gameworld/entities/Stick.java
@@ -0,0 +1,58 @@
+package gameworld.entities;
+
+import java.util.List;
+
+import gameworld.Location.Direction;
+import gameworld.entities.Item.Action;
+
+public class Stick extends EquipableStrategy {
+
+	@Override
+	public List<String> getActions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isSolid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isDoor() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Direction getDirection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDirection(Direction direction) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String performAction(Action action) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/gameworld/entities/Tree.java
@@ -0,0 +1,72 @@
+package gameworld.entities;
+
+import java.util.List;
+
+import gameworld.Location;
+import gameworld.Location.Direction;
+import gameworld.Room;
+import gameworld.entities.Item.Action;
+
+public class Tree extends AttackingEntity implements Strategy{
+
+	public Tree() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void die() {
+		// remove tree from world and replace with drop
+		Room currentRoom = Player.getInstance().getCurrentRoom();
+		Location treeLoc = currentRoom.getGameItemLocation(new Item(this));
+		currentRoom.addGameObject(treeLoc, new Item (new Stick()));
+	}
+
+	@Override
+	public List<String> getActions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isSolid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isDoor() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Direction getDirection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDirection(Direction direction) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String performAction(Action action) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
