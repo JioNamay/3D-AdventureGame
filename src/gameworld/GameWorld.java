2230ebc42306c1ed5ca7e2ab35067568125c9156
Carrie Yang <yangcarr@myvuw.ac.nz>
2018-10-14 17:02:29 +1300

    constant fields representing room names are included in AdventureGame.java. A method for setting up each rooms are also included, but haven't been implemented yet.

--- src/gameworld/GameWorld.java
+++ src/gameworld/GameWorld.java
@@ -6,16 +6,29 @@ import gameworld.entities.Player;
  * GameWorld handles all the entities and items.
  */
 public class GameWorld {
+	// Rooms in the GameWorld:
+	public static final String LIBRARY = "library";
+	public static final String GUEST_BEDROOM = "Guest Bedroom";
+	public static final String FOYER = "Foyer";
+	public static final String COURTYARD = "Courtyard";
+	public static final String MASTER_BEDROOM = "Master Bedroom";
+	public static final String KITCHEN = "Kitchen";
 
 	private Player player = Player.getInstance();
+	private Room currentRoom;
 
 	/**
 	 * Instantiates a new game world.
-	 *
+	 * The player starts off in the courtyard.
 	 */
 	public GameWorld() {
-		Room currentRoom = new Room("Test Room");
-
+		setUpWorld();	// sets out the layout of the gameworld
+		// current room is the courtyard
+		// set player in the courtyard
+	}
+	
+	public void setUpWorld() {
+		
 	}
 
 	/**
