7a96ad1634b054c19931cad5adea9239d44aaf1a
Deanne Alabastro <aladeanra@gmail.com>
2018-10-14 22:16:52 +1300

    Override performAction

--- src/gameworld/entities/Sofa.java
+++ src/gameworld/entities/Sofa.java
@@ -5,6 +5,7 @@ package gameworld.entities;
 
 import gameworld.Location;
 import gameworld.Room;
+import gameworld.entities.Item.Action;
 
 /**
  * Sofa can be attacked and gives players coins when it disappears.
@@ -34,5 +35,39 @@ public class Sofa extends Monster{
 		currentRoom.removeGameItem(sofaLoc);
 		Player.getInstance().addCoins(20); // give player coins
 	}
+	
+	@Override
+	public String performAction(Action action) {
+		switch (action) {
+		case EXAMINE:
+			return description;
+		case ATTACK:
+			EquipableStrategy weapon = Player.getInstance().getEquippedWeapon();
+			if (weapon != null)
+				weapon.getDamaged(1); // decrease durability of player's weapon
+			int damage = Player.getInstance().attack(this); // get attacked by player
+			
+			// result of the player's attack
+			String result = null;
+			if (damage > 0)
+				result = "Player caused " + damage + " damage to " + this.name + "'s life";
+			else
+				result = "Player's attack missed and caused no damage";
+
+			// result of the item's retaliation
+			String retaliationStr = null;
+			damage = this.attack(Player.getInstance());
+			if (damage > 0)
+				retaliationStr = "The " + this.name
+						+ " shook from the attack attempt and moved towards the player. The player stubbed their toe, causing "
+						+ damage + "damage.";
+			else
+				retaliationStr = "The " + this.name
+						+ " moved and almost hit your knee. It caused no damage.";
+			return result + "\n" + retaliationStr;
+
+		default:
+			throw new IllegalArgumentException("Unknown action: " + action.toString() + " for object: "+ this.name);
+		}
 
 }
