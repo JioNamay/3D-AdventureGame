2577a12c756caac259c97b39e0e1460fef41252d
Deanne Alabastro <aladeanra@gmail.com>
2018-10-14 18:38:55 +1300

    Add comments

--- src/gameworld/entities/EquipableStrategy.java
+++ src/gameworld/entities/EquipableStrategy.java
@@ -5,19 +5,39 @@ import java.util.List;
 
 import gameworld.entities.Item.Action;
 
+/**
+ * The EquipableStrategy is for every item that has the capability to be a weapon.
+ * @author Deanne Alabastro 300346210
+ */
 public abstract class EquipableStrategy extends PickUpAbleStrategy implements Damageable {
 
+	/** The max damage. */
 	protected int maxDamage;
+	
+	/** The durability. */
 	protected int durability;
 
+	/**
+	 * Gets the attack damage.
+	 *
+	 * @return the attack damage
+	 */
 	public int getAttackDamage() {
 		return maxDamage;
 	}
 
+	/**
+	 * Sets the attack damage.
+	 *
+	 * @param maxDamage the new attack damage
+	 */
 	public void setAttackDamage(int maxDamage) {
 		this.maxDamage = maxDamage;
 	}
-
+ 
+	/* (non-Javadoc)
+	 * @see gameworld.entities.PickUpAbleStrategy#performAction(gameworld.entities.Item.Action)
+	 */
 	@Override
 	public String performAction(Action action) {
 		switch (action) {
@@ -38,6 +58,9 @@ public abstract class EquipableStrategy extends PickUpAbleStrategy implements Da
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see gameworld.entities.Strategy#getActions()
+	 */
 	@Override
 	public List<String> getActions() {
 		if (!Player.getInstance().getInventory().contains(this))
@@ -47,21 +70,33 @@ public abstract class EquipableStrategy extends PickUpAbleStrategy implements Da
 		return Arrays.asList(Action.EXAMINE.toString(), Action.EQUIP.toString(), Action.DROP.toString());
 	}
 
+	/* (non-Javadoc)
+	 * @see gameworld.entities.Strategy#isSolid()
+	 */
 	@Override
 	public boolean isSolid() {
 		return false;
 	}
 
+	/* (non-Javadoc)
+	 * @see gameworld.entities.Strategy#isDoor()
+	 */
 	@Override
 	public boolean isDoor() {
 		return false;
 	}
 
+	/* (non-Javadoc)
+	 * @see gameworld.entities.Damageable#getHealth()
+	 */
 	@Override
 	public int getHealth() {
 		return durability;
 	}
 
+	/* (non-Javadoc)
+	 * @see gameworld.entities.Damageable#getDamaged(int)
+	 */
 	@Override
 	public void getDamaged(int amount) {
 		if ((this.durability - amount) < 0) {
@@ -72,6 +107,9 @@ public abstract class EquipableStrategy extends PickUpAbleStrategy implements Da
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see gameworld.entities.Damageable#die()
+	 */
 	@Override
 	public void die() {
 		Player.getInstance().getInventory().remove(this);
