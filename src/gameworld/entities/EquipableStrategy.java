d9f1d80440409e179fbdc8162ed15907c95b3d62
Deanne Alabastro <aladeanra@gmail.com>
2018-10-14 16:21:01 +1300

    added option to unequip

--- src/gameworld/entities/EquipableStrategy.java
+++ src/gameworld/entities/EquipableStrategy.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import gameworld.entities.Item.Action;
 
-public abstract class EquipableStrategy extends PickUpAbleStrategy implements Damageable{
+public abstract class EquipableStrategy extends PickUpAbleStrategy implements Damageable {
 
 	protected int maxDamage;
 	protected int durability;
@@ -13,27 +13,37 @@ public abstract class EquipableStrategy extends PickUpAbleStrategy implements Da
 	public int getAttackDamage() {
 		return maxDamage;
 	}
-	
+
 	public void setAttackDamage(int maxDamage) {
 		this.maxDamage = maxDamage;
 	}
-	
+
 	@Override
 	public String performAction(Action action) {
-		switch(action) {
+		switch (action) {
 		case EXAMINE:
 			return givePlayerCoins(1);
+		case DROP:
+			if(Player.getInstance().getEquipedWeapon().equals(this)) {
+				return this.performAction(Action.UNEQUIP) + "\n" + drop();
+			}
 		case EQUIP:
 			Player.getInstance().setEquipedWeapon(this);
-			return "Player equiped " + this.name;
+			return "Player equipped " + this.name;
+		case UNEQUIP:
+			Player.getInstance().setEquipedWeapon(null);
+			return "Player unequipped " + this.name;
 		default:
 			return super.performAction(action);
-		}	
+		}
 	}
 
 	@Override
 	public List<String> getActions() {
-		if(!Player.getInstance().getInventory().contains(this)) return actions; // return default actions if not in inventory
+		if (!Player.getInstance().getInventory().contains(this))
+			return actions; // return default actions if not in inventory
+		if (Player.getInstance().getEquipedWeapon().equals(this)) //if equipped
+			return Arrays.asList(Action.EXAMINE.toString(), Action.UNEQUIP.toString(), Action.DROP.toString());
 		return Arrays.asList(Action.EXAMINE.toString(), Action.EQUIP.toString(), Action.DROP.toString());
 	}
 
@@ -54,14 +64,14 @@ public abstract class EquipableStrategy extends PickUpAbleStrategy implements Da
 
 	@Override
 	public void getDamaged(int amount) {
-		if((this.durability - amount) < 0) {
+		if ((this.durability - amount) < 0) {
 			this.durability = 0;
 			this.die();
-		}else {
+		} else {
 			durability -= amount;
 		}
 	}
-	
+
 	@Override
 	public void die() {
 		Player.getInstance().getInventory().remove(this);
